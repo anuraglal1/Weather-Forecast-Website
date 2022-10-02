2bf8c7ea9011faca302ee1aab2049600d3fc324e
toadwx <toad1225@gmail.com>
2022-10-02 14:44:54 -0400

    Minor updates to temperature template. Adds icons for some current conditions. Adds template and static directories (required for Flask)

--- Weather.py
+++ Weather.py
@@ -3,40 +3,41 @@
 from flask import Flask, render_template, request, url_for
 import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/temperature',methods=['POST'])
 def temperature():
-    city=request.form['city']
-    r=requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=01d1f6ccaffe5c8f36a6196d7c7485a6')
-    #return r.text
-    json_object=r.json()
-    weather=json_object['weather'][0]['main']
-    #return weather
-    temp_k=float(json_object['main']['temp'])
-    #return str(temp_k)
-    temp_f=int(temp_k-273.15)
-    return render_template('temperature.html',temp=temp_f,weather=weather)
+    city = request.form['city']
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=01d1f6ccaffe5c8f36a6196d7c7485a6')
+
+    json_object = r.json()
+    
+    weather = json_object['weather'][0]['main']
+    temp_k = float(json_object['main']['temp'])
+    temp_c = int(temp_k-273.15)
+    
+    return render_template('temperature.html', temp=temp_c, weather=weather, city=city.capitalize())
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
 
 
 #Using ZipCode
-
-
-@app.route('/temperature_celsius', methods=['POST'])
+@app.route('/temperature_fahrenheit', methods=['POST'])
 def temperature_celsius():
    zipcode = request.form['zip']
    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=01d1f6ccaffe5c8f36a6196d7c7485a6')
+   
    json_object = r.json()
+   
+   weather = json_object['weather'][0]['main']
    temp_k = float(json_object['main']['temp'])
-   #return str(temp_k)
    temp_f = (temp_k - 273.15) * 1.8 + 32
-   return render_template('temperature.html', temp=temp_k)
+   
+   return render_template('temperature.html', temp=temp_f, weather=weather, city=city.capitalize())
 
 
